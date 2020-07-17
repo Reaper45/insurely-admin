5e0365444a524f2fc64725be8173c091d44a252c
Paul Bombo <pbombo@gmail.com>
2020-07-17 09:17:09 +0300

    feat: Added MFA table

---
+++ database/migrations/2020_07_17_054600_create_user_mfa_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMFADetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_mfa_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string("mfa_type")->unique();
+            $table->string("secret");
+            $table->timestamp("expires_in");
+            $table->timestamps();
+
+            $table->foreign('user_id', 'pt_foreign_user')
+                    ->references('id')
+                    ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_mfa_details', function (Blueprint $table) {
+            $table->dropForeign('pt_foreign_users');
+        });        
+        Schema::dropIfExists('user_mfa_details');
+    }
+}
